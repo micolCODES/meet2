fc6c650bdcfcb20b6136610268fe6f2410fa2e8b
micolCODES <micolromano124@gmail.com>
2022-10-10 21:44:27 +0200

    4.3 WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I edited <code>src/App.js</code> and this is what I wrote.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EventList />
+      <CitySearch />
     </div>
   );
 }
