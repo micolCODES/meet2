62ad137152d979d237c59c6451fcc58a5e81c827
micolCODES <micolromano124@gmail.com>
2022-10-14 09:41:56 +0200

    Updates

---
+++ precache-manifest.c4cccb9ff5acfa91542751c7223f3d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74733de897f0ff981027",
+    "url": "/meet/static/css/main.734570df.chunk.css"
+  },
+  {
+    "revision": "74733de897f0ff981027",
+    "url": "/meet/static/js/main.849d246a.chunk.js"
+  },
+  {
+    "revision": "87b458b32d0c33dc517c",
+    "url": "/meet/static/js/runtime~main.bc0c07f8.js"
+  },
+  {
+    "revision": "24d8d31368cf7cf296ac",
+    "url": "/meet/static/js/2.6bc8e851.chunk.js"
+  },
+  {
+    "revision": "59c80747cb44ac3d4e18",
+    "url": "/meet/static/js/3.34e2ee7d.chunk.js"
+  },
+  {
+    "revision": "5d9890eed79cfb2901d072feed4394f1",
+    "url": "/meet/index.html"
+  }
+];
